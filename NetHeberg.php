50cb390887117947c6bbc52a51b3d4e98e080aa5
Valentin Grtz <39189263+ValentinGratz@users.noreply.github.com>
2020-12-25 11:44:10 +0100

    Add files via upload

---
+++ NetHeberg.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Plugin Name: Welcome NetHeberg WP
+Plugin URI:
+Description: Merci d'avoir choisi NetHeberg pour héberger votre site WordPress
+Author: Valentin Gratz
+Version: 0.0.1
+Author URI: http://www.valentin-gratz.xyz
+*/
+
+// crochet qui va executer generateCSS
+function generateCSS(){
+    wp_register_style('monCSSBootstrap', plugins_url('NetHebergWelcomeWP/assets/bootstrap.css'));
+    wp_register_style('monCSS', plugins_url('NetHebergWelcomeWP/assets/style.css'));
+}
+
+
+add_action( 'admin_head', 'generateCSS' );
+
+function displayAdminMenu(){
+
+    wp_enqueue_style('monCSSBootstrap');
+    wp_enqueue_style('monCSS');
+
+    echo "
+        <center><h1>Merci d'avoir choisi NetHeberg pour votre site !</h1></center>
+        <br>
+        <br><center>
+              <!-- <div class=\"akismet-masthead__logo-container\">-->
+				<img class=\"balimgbr\" src=\"<?php echo esc_url( plugins_url( './assets/img/logo_netheberg.png', __FILE__ ) ); ?>\" alt=\"logo NetHeberg\" />
+			  <!--  </div>--></center>
+        <br>
+        <br>
+        <h2>Nous retrouver !</h2>
+        <p></p>
+            <ul><li>Site internet : <a href='http://www.netheberg.fr'>NetHeberg.fr</a></li>
+                <li>Serveur Discord : <a href='https://discord.gg/j5yjUsKVWC'>https://discord.gg/j5yjUsKVWC</a></li></ul>
+        <br><br><br><br>
+        <div align=\"right\">Version 0.0.1</div>
+    ";
+}
+
+function createMenu(){
+    add_menu_page("NetHeberg Welcome",
+        "NetHeberg Welcome",
+        "manage_options",
+        "NetHeberg Welcome",
+        "displayAdminMenu",
+        "",
+        1);
+
+}
+
+ add_action('admin_menu', "createMenu"); //mettre en dehors de la function
---
+++ README.md
@@ -0,0 +1,8 @@
+# SupWPPlugin
+ Mon premier plugin WordPress
+
+Fait durant la formation DWWM
+
+Aide de la documentation suivante : https://developer.wordpress.org/plugins/administration-menus/top-level-menus/
+
+# #100DaysOfCode
